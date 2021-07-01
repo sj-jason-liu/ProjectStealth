e35001139fd5319d556cb83fec5c98c2e2e4f4b7
sj-jason-liu <sj.jason.liu@gmail.com>
2021-07-01 15:25:03 +0800

    Created all cutscenes & Added NavMeshAgent to Player with mouse clicking movement function & Camera switching by player position.

---
+++ Assets/The Great Fleece/Game/Scripts/CameraTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTrigger : MonoBehaviour
+{
+    //check for trigger of player
+    //update the main camera to direct angle
+    [SerializeField]
+    private GameObject _camera;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Camera.main.transform.position = _camera.transform.position;
+            Camera.main.transform.rotation = _camera.transform.rotation;
+        }
+    }
+}
---
+++ Assets/The Great Fleece/Game/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour
+{
+    //create variable to storage navmeshagent
+    private NavMeshAgent _navMeshAgent;
+    
+    void Start()
+    {
+        //assign navmeshagent
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit));
+            {
+                _navMeshAgent.SetDestination(hit.point);
+            }
+        }
+    }
+}
