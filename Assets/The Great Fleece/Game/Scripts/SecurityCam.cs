6c1c60cd161dd528fda6177f4a27a6425a5be1be
sj-jason-liu <sj.jason.liu@gmail.com>
2021-07-06 18:51:47 +0800

    Created the function of security cameras with moving animation & detecting collider.

---
+++ Assets/The Great Fleece/Game/Scripts/SecurityCam.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecurityCam : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _gameoverCutscene;
+
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponentInParent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Color _color = new Color(0.6f, 0f, 0f, 0.03f);
+            GetComponent<MeshRenderer>().material.SetColor("_TintColor", _color);
+            _animator.enabled = false;
+            Invoke("EndingCutscene", 0.5f);
+        }    
+    }
+
+    void EndingCutscene()
+    {
+        _gameoverCutscene.SetActive(true);
+    }
+}
