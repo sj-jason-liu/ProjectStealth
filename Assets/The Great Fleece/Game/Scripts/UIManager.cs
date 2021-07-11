3a90259d94b9416b271accf4556246de4cf2025e
sj-jason-liu <sj.jason.liu@gmail.com>
2021-07-11 13:34:02 +0800

    Added UIManager with restart & quit options.

---
+++ Assets/The Great Fleece/Game/Scripts/UIManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    private static UIManager _instance;
+    public static UIManager Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                Debug.Log("UIManager is Missing!");
+            }
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    public void RestartGame()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.buildIndex);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
